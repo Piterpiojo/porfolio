9a9b4b1226a7abf53420a799560593f47ccbb442
Piterpiojo <delgado.aldo87@gmail.com>
2022-08-14 22:51:12 -0300

    modificados:     src/app/acerca-de/acerca-de.component.css modificados:     src/app/acerca-de/acerca-de.component.html modificados:     src/app/acerca-de/acerca-de.component.ts modificados:     src/app/educacion/educacion.component.ts modificados:     src/app/experiencia/experiencia.component.html modificados:     src/app/experiencia/experiencia.component.ts modificados:     src/app/habilidades/habilidades.component.css modificados:     src/app/habilidades/habilidades.component.html borrados:        src/app/modelo/experienciaI.ts modificados:     src/app/proyectos/proyectos.component.css modificados:     src/app/proyectos/proyectos.component.html modificados:     src/app/proyectos/proyectos.component.ts modificados:     src/app/servicios/portfolio.service.ts src/app/modelo/Estudios.ts src/app/modelo/Habilidad.ts src/app/modelo/Proyecto.ts src/app/modelo/persona.ts

--- src/app/habilidades/habilidades.component.ts
+++ src/app/habilidades/habilidades.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { PortfolioService } from '../servicios/portfolio.service';
+import {Habilidad} from '../modelo/Habilidad';
 
 @Component({
   selector: 'app-habilidades',
@@ -8,17 +10,54 @@ import { PortfolioService } from '../servicios/portfolio.service';
 })
 export class HabilidadesComponent implements OnInit {
 miPortfolio:any;
+persona_id:number=-1;
+mostrar:number=-1;
+formEditar= new FormGroup({
+nombre: new FormControl('',Validators.required),
+valor:new FormControl('',Validators.required)
+})
   constructor(private datosPorfolio:PortfolioService) { }
 
   ngOnInit(): void {
     this.datosPorfolio.obtenerDatos().subscribe(data => {
+      this.persona_id = data.persona_id;
       this.miPortfolio=data.lhabl;
 
     })
   }
 
 
-  id(a:string){
-    console.log("clikeaste " + a);
+
+  funcmostrar(exp:number){
+    if (this.mostrar !== exp){
+      this.mostrar=exp;
+    }else{
+      this.mostrar= -1;
+    }
+
   }
+
+
+agregar(form:Habilidad){
+form.persona_id=this.persona_id;
+this.datosPorfolio.agregarHab(form).subscribe(data =>{
+console.log(data);
+});
+}
+
+eliminar(id:number){
+  this.datosPorfolio.eliminarHab(id).subscribe(data =>{
+    console.log(data);
+  })
+}
+
+editar(form:Habilidad,id:number){
+form.persona_id=this.persona_id;
+this.datosPorfolio.editarHab(form,id).subscribe(data =>{
+  console.log(data);
+})
+this.mostrar = -1;
+}
+
+
 }
